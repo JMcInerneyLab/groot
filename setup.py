23c59c0be467b732ea9d13e262e8341380e4a4e6
Martin <martinrusilowicz@m-ul-mbmhsmr2.it.manchester.ac.uk>
2017-10-26 12:35:35 +0100

    version number

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from distutils.core import setup
 
 
 setup( name = "groot",
-       version = "0.0.0.1",
+       version = "0.0.0.2",
        description = "Generate N-rooted fusion graphs from genomic data.",
        author = "Martin Rusilowicz",
-       long_description = open( "readme.md" ).read(),
        license = "GPLv3",
        packages = [ "groot" ],
        entry_points= { "console_scripts": [ "groot = groot.__main__:main" ] }
