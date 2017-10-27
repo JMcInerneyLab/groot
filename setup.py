d5b1e7528740a7bbf08bfcfb1869e3412529bb0b
Martin <martinrusilowicz@m-ul-mbmhsmr2.it.manchester.ac.uk>
2017-10-27 12:24:22 +0100

    setup.py in favour of requirements.txt

--- setup.py
+++ setup.py
@@ -3,10 +3,20 @@ from distutils.core import setup
 
 setup( name = "groot",
        url="https://bitbucket.org/mjr129/groot",
-       version = "0.0.0.2",
+       version = "0.0.0.3",
        description = "Generate N-rooted fusion graphs from genomic data.",
        author = "Martin Rusilowicz",
        license = "GPLv3",
-       packages = [ "groot" ],
-       entry_points= { "console_scripts": [ "groot = groot.__main__:main" ] }
+       packages = [ "groot",
+                    "groot.algorithms",
+                    "groot.data",
+                    "groot.extensions",
+                    "groot.frontends",
+                    "groot.frontends.cli",
+                    "groot.frontends.gui",
+                    "groot.frontends.gui.forms",
+                    "groot.frontends.gui.forms.designer",
+                    ],
+       entry_points= { "console_scripts": [ "groot = groot.__main__:main" ] },
+       install_requires = ["ete3"]
        )
