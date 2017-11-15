9a342ca39379e321c1518be7eb82924322391791
Martin <martinrusilowicz@m-ul-mbmhsmr2.it.manchester.ac.uk>
2017-11-15 14:48:31 +0000

    fixed setup

--- groot/__init__.py
+++ groot/__init__.py
@@ -1,10 +1,13 @@
 from intermake import MENV, create_simple_host_provider_from_class, ConsoleHost
 from groot.extensions import ext_viewing, ext_files, ext_generating, ext_gimmicks, ext_modifications
 
+
 def __gui_host():
     from groot.frontends.gui.gui_host import LegoGuiHost
     return LegoGuiHost()
 
+
 MENV.name = "GROOT"
 MENV.abv_name = "groot"
 MENV.host_provider = create_simple_host_provider_from_class( ConsoleHost, __gui_host )
+MENV.lock()
