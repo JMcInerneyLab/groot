2603bf28f1d2e18abeb34df61fd5c4453571cbb3
Martin <martinrusilowicz@m-ul-mbmhsmr2.it.manchester.ac.uk>
2017-11-23 14:42:01 +0000

    cc: pep8 version number

--- groot/__init__.py
+++ groot/__init__.py
@@ -2,7 +2,7 @@ from intermake import MENV, create_simple_host_provider_from_class, ConsoleHost
 from groot.extensions import ext_viewing, ext_files, ext_generating, ext_gimmicks, ext_modifications
 
 __author__ = "Martin Rusilowicz"
-__version__ = "0.0.0.9"
+__version__ = "0.0.0.10"
 
 def __gui_host():
     from groot.frontends.gui.gui_host import LegoGuiHost
@@ -11,6 +11,5 @@ def __gui_host():
 
 MENV.name = "GROOT"
 MENV.abv_name = "groot"
-MENV.version = "0.0.0.0"
+MENV.version = __version__
 MENV.host_provider = create_simple_host_provider_from_class( ConsoleHost, __gui_host )
-
