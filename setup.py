74fad3bed43c16a202059b15c0b7aa9891a52901
Martin <martinrusilowicz@m-ul-mbmhsmr2.it.manchester.ac.uk>
2018-11-13 17:25:20 +0000

    included classifiers in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup( name = "groot",
        url = "https://bitbucket.org/mjr129/groot",
-       version = "0.0.0.57",
+       version = "0.0.0.58",
        description = "Generate N-rooted fusion graphs from genomic data.",
        author = "Martin Rusilowicz",
        license = "https://www.gnu.org/licenses/agpl-3.0.html",
@@ -37,5 +37,39 @@ setup( name = "groot",
                            "editorium",
                            "six",  # groot doesn't use this, but ete needs it
                            ],
-       python_requires = ">=3.6"
+       python_requires = ">=3.6",
+
+       classifiers = \
+           [
+               "Development Status :: 3 - Alpha",
+        
+               "Environment :: Console",
+               "Environment :: Win32 (MS Windows)",
+               "Environment :: X11 Applications :: Qt",
+               "Environment :: MacOS X",
+               "Operating System :: OS Independent",
+               "Operating System :: Microsoft :: Windows",
+               "Operating System :: MacOS",
+               "Operating System :: POSIX :: Linux",
+        
+               "Intended Audience :: Science/Research",
+               "Topic :: Scientific/Engineering",
+               "Topic :: Scientific/Engineering :: Bio-Informatics",
+               "Topic :: Utilities",
+               "Topic :: Terminals",
+               "Topic :: Multimedia :: Graphics :: Editors",
+               "Topic :: Multimedia :: Graphics :: Editors :: Vector-Based",
+               "Topic :: Multimedia :: Graphics :: Presentation",
+               "Topic :: Multimedia :: Graphics :: Viewers",
+        
+        
+               "License :: OSI Approved :: GNU Affero General Public License v3",
+               "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)"
+        
+               "Natural Language :: English",
+               "Programming Language :: Python :: 3.6",
+               "Programming Language :: Python :: 3",
+               "Programming Language :: Python :: 3 :: Only",
+               "Programming Language :: Other Scripting Engines"
+           ]
        )
